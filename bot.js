68d6eeebbeb9d9787d3656dd3ca7521c8fcdd6af
Yurii Shyba <yuriy.shyba@gmail.com>
2022-03-20 00:12:21 +0200

    code

---
+++ bot.js
@@ -0,0 +1,28 @@
+const {Telegraf} = require('telegraf')
+const token = '5282404263:AAF-DrOmyBZ1bc1-vwUBjKBSQDEWdiEKqSU';
+const bot = new Telegraf(token);
+bot.context.db = {
+    countingForDeadInsides: 0
+}
+bot.hears('1000-7',ctx=>{
+    async function deadIndide(){
+        for(let i=1000;i>0;i=i-7){
+            ctx.reply(i);
+            await sleep(3000);
+        }
+        ctx.reply('DEAD INSIDE!!!!!');
+        bot.context.db.countingForDeadInsides = 0;
+    }
+    if(bot.context.db.countingForDeadInsides === 0){
+        deadIndide();
+        bot.context.db.countingForDeadInsides = 1;
+    }
+})
+
+// bot.telegram.setWebhook('https://mysterious-mesa-16110.herokuapp.com/' + token)
+
+bot.launch();
+
+function sleep(ms){
+    return new Promise(resolve=>setTimeout(resolve,ms));
+}
