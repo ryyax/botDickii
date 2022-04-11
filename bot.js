c2b8f13fee71d17813c4279ceee98ea83c744b89
Yuriy <yuriy.shyba@gmail.com>
2022-04-11 10:55:03 +0300

    1

--- bot.js
+++ bot.js
@@ -16,6 +16,7 @@ const dickaya_genshtab = -699023771;
 let db = {
 
 }
+client.connect();
 
 // technical functions
 function sleep(ms){
@@ -222,7 +223,6 @@ bot.command('/weather', ctx=>{
     daily_weather_lviv(ctx.message.chat.id)
 })
 bot.command('addvoice', async ctx=>{
-    await client.connect();
     let voice_message_name = ctx.message.text.replace(/\/addvoice.* /, '').toLowerCase();
     let voice_message_list = [];
     let voice_message_list_id = 'voice_message_list' + ctx.message.chat.id;
@@ -256,10 +256,8 @@ bot.command('addvoice', async ctx=>{
         // reply(ctx, 'Дай відповідь на голосове повідомлення, яке бажаєш зберегти') 
         reply(ctx, 'я шо Ванга, шоб знати яке саме голосове ти хцеш добавити?...')
     }
-    await client.disconnect();
 })
 bot.command('voicelist', async (ctx)=>{
-    await client.connect()
     let voice_message_list_id = 'voice_message_list' + ctx.message.chat.id;
     let promise = new Promise((resolve,reject)=>{
         resolve(client.get(voice_message_list_id));
@@ -270,7 +268,6 @@ bot.command('voicelist', async (ctx)=>{
     } else{
         reply(ctx, 'У вас не додано жодного голосового повідомлення. Скористайтесь командою /addvoice, для того, щоб добавити голосове повідомлення')
     }
-    await client.disconnect();
 })
 bot.command('delvoice', ctx=>{
     reply(ctx, 'в розробці. деліт ше не завезли')
@@ -285,7 +282,6 @@ bot.hears('test',ctx=>{
 // bot.on('sticker', ctx => reply(ctx, 'заєбеш'))
 bot.on('voice', ctx => reply(ctx,'блять в тебе шо букви платні?'))
 bot.on('text', async ctx=>{
-    await client.connect();
     let promise = new Promise((resolve)=>{
         resolve(client.get('voice_message_list' + ctx.message.chat.id))
     })
@@ -300,7 +296,6 @@ bot.on('text', async ctx=>{
             ctx.replyWithVoice(voice_message_id);
         }    
     } 
-    await client.disconnect();
 })
 
 
